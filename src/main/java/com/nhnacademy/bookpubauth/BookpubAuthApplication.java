44ec15baf6b282a6def83302e17a4e7c23338599
정유진 <76966467+hobby3530@users.noreply.github.com>
2023-01-02 22:22:33 +0900

    [Chore] 프로젝트 생성
    bookpub-auth 프로젝트 생성

---
+++ src/main/java/com/nhnacademy/bookpubauth/BookpubAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpubAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/bookpubauth/BookpubAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpubAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
