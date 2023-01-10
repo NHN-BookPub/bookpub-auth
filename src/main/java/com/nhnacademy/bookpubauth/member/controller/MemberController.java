6f216a22bc913687067cc71a0f397008c702a545
limtaewon <tagkdj1@gmail.com>
2023-01-10 16:15:31 +0900

    [Feat] 토큰발행 controller 생성
    api 따라 토큰 발행, 재발행 api 생성

---
+++ src/main/java/com/nhnacademy/bookpubauth/member/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.bookpubauth.member.controller;
+
+import com.nhnacademy.bookpubauth.member.dto.LoginMemberRequestDto;
+import com.nhnacademy.bookpubauth.member.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * jwt 발급을 위한 컨트롤러.
+ *
+ * @author : 임태원
+ * @since : 1.0
+ **/
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/token")
+public class MemberController {
+    private final TokenService tokenService;
+
+    @GetMapping("/issue")
+    public ResponseEntity<String> tokenIssued(@RequestBody LoginMemberRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(tokenService.tokenIssued(requestDto.getUserId(), requestDto.getAuthorities()));
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<String> tokenReIssued(@RequestBody LoginMemberRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(tokenService.tokenReIssued(requestDto.getUserId(), requestDto.getAuthorities()));
+    }
+}
