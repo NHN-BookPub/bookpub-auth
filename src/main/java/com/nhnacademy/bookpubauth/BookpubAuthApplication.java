a3be74e80fafa8461c81bccf1afcac9f9e87404e
limtaewon <tagkdj1@gmail.com>
2023-01-12 21:48:05 +0900

    [Refactor] 기능 추가
    PropertiesScan 어노테이션 추가

--- src/main/java/com/nhnacademy/bookpubauth/BookpubAuthApplication.java
+++ src/main/java/com/nhnacademy/bookpubauth/BookpubAuthApplication.java
@@ -2,10 +2,11 @@ package com.nhnacademy.bookpubauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BookpubAuthApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookpubAuthApplication.class, args);
 	}
