64e9ce28fba17987eccaeb576acbb027b6dd378d
limtaewon <tagkdj1@gmail.com>
2023-01-14 21:45:24 +0900

    [Refactor] filter 구조변경
    front에서 requestBody에 정보를 담아줌에 따라 코드 변경

--- src/main/java/com/nhnacademy/bookpubauth/member/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/nhnacademy/bookpubauth/member/filter/CustomAuthenticationFilter.java
@@ -1,6 +1,12 @@
 package com.nhnacademy.bookpubauth.member.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.bookpubauth.member.dto.LoginMemberRequestDto;
+import com.nhnacademy.bookpubauth.member.exception.UserDtoParsingException;
 import com.nhnacademy.bookpubauth.member.provider.CustomAuthenticationProvider;
+import com.nhnacademy.bookpubauth.token.service.TokenService;
+import java.io.IOException;
+import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,38 +21,43 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  *
  * @author : 임태원
  * @since : 1.0
-
  **/
 @Slf4j
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final CustomAuthenticationProvider provider;
+    private final TokenService tokenService;
+    private final ObjectMapper objectMapper;
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String TOKEN_TYPE = "Bearer ";
+    private String sessionId;
 
     @Override
     public Authentication attemptAuthentication(
             HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        LoginMemberRequestDto requestDto;
 
-        String userId = this.obtainUsername(request);
-        userId = userId != null ? userId.trim() : "";
-        String pwd = this.obtainPassword(request);
-        pwd = pwd != null ? pwd : "";
-
-        log.warn(userId);
-        log.warn(pwd);
+        try {
+            requestDto = objectMapper.readValue(request.getInputStream(), LoginMemberRequestDto.class);
+            this.sessionId = requestDto.getSessionId();
+        } catch (IOException e) {
+            throw new UserDtoParsingException();
+        }
 
         UsernamePasswordAuthenticationToken token
-                = new UsernamePasswordAuthenticationToken(userId,pwd);
+                = new UsernamePasswordAuthenticationToken(requestDto.getId(), requestDto.getPwd());
+
 
         return provider.authenticate(token);
     }
 
     @Override
-    protected String obtainPassword(HttpServletRequest request) {
-        return request.getParameter("pwd");
+    protected void successfulAuthentication(
+            HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+        String accessToken = tokenService.tokenIssued(
+                (Long) authResult.getPrincipal(), this.sessionId, authResult.getAuthorities());
+
+        response.setHeader(AUTH_HEADER, TOKEN_TYPE + accessToken);
     }
 
-    @Override
-    protected String obtainUsername(HttpServletRequest request) {
-        return request.getParameter("id");
-    }
 }
