6c7558e12953ce801bb93df6011f8b8c2e2536f2
limtaewon <tagkdj1@gmail.com>
2023-01-14 22:02:50 +0900

    [Docs] 자바독 작성

--- src/main/java/com/nhnacademy/bookpubauth/token/controller/TokenController.java
+++ src/main/java/com/nhnacademy/bookpubauth/token/controller/TokenController.java
@@ -6,7 +6,6 @@ import com.nhnacademy.bookpubauth.token.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth/token")
 public class TokenController {
     private final TokenService tokenService;
-
-    @PostMapping("/issue")
-    public ResponseEntity<MemberInfoResponseDto> tokenIssued(@RequestBody MemberInfoRequestDto requestDto) {
-        return null;
-    }
 
     @GetMapping("/reissue")
     public ResponseEntity<MemberInfoResponseDto> tokenReIssued(@RequestBody MemberInfoRequestDto requestDto) {
