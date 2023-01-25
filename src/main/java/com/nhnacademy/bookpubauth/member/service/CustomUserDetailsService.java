fa11efac8fc5eaec5a67187d8843271b68eec787
limtaewon <tagkdj1@gmail.com>
2023-01-25 12:28:31 +0900

    [Refactor] 코드 수정
    Security 의 User 객체에 맞게 수정

--- src/main/java/com/nhnacademy/bookpubauth/member/service/CustomUserDetailsService.java
+++ src/main/java/com/nhnacademy/bookpubauth/member/service/CustomUserDetailsService.java
@@ -1,22 +1,25 @@
 package com.nhnacademy.bookpubauth.member.service;
 
 
-import com.nhnacademy.bookpubauth.member.CustomUserDetails;
 import com.nhnacademy.bookpubauth.member.adaptor.MemberAdaptor;
 import com.nhnacademy.bookpubauth.member.dto.MemberInfoRequestDto;
 import com.nhnacademy.bookpubauth.member.dto.MemberInfoResponseDto;
 import com.nhnacademy.bookpubauth.member.exception.NotMemberBookpubSite;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-/** UserDetailService 클래스 상속받아 커스터마이징 클래스.
- *
+/**
+ * UserDetailService 클래스 상속받아 커스터마이징 클래스.
  */
 @Service
 @RequiredArgsConstructor
@@ -24,7 +27,8 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
     private final MemberAdaptor memberAdaptor;
 
-    /** 사용자 정보를 담는 인터페이스인 UserDetail을 반환해주는 메소드.
+    /**
+     * 사용자 정보를 담는 인터페이스인 UserDetail을 반환해주는 메소드.
      *
      * @param userId 로그인 한 user의 아이디.
      * @return 사용자 정보가 담겨있는 UserDetail 클래스를 상속받은 Custom클래스.
@@ -38,7 +42,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (Objects.isNull(responseMemberData)) {
             throw new NotMemberBookpubSite();
         }
+        MemberInfoResponseDto member = responseMemberData.getBody();
 
-        return new CustomUserDetails(responseMemberData.getBody());
+        List<SimpleGrantedAuthority> grantedAuthorities =
+                member.getAuthorities().stream().map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        return new User(member.getMemberNo().toString(), member.getMemberPwd(), grantedAuthorities);
     }
 }
