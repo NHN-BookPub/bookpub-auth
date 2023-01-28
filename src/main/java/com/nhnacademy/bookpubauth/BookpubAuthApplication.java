1da3efdaa76728c9a597ec0424da7ea185eca50b
limtaewon <tagkdj1@gmail.com>
2023-01-28 23:16:41 +0900

    [Docs] 자바독 추가

--- src/main/java/com/nhnacademy/bookpubauth/BookpubAuthApplication.java
+++ src/main/java/com/nhnacademy/bookpubauth/BookpubAuthApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * auth 서버 메인.
+ */
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BookpubAuthApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BookpubAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookpubAuthApplication.class, args);
+    }
 
 }
